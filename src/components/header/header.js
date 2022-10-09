bb2d9a251098538747e7be686e30bb6b9c1c3a32
Delphine <delphine.pva@gmail.com>
2022-10-09 17:18:43 +0200

    Configuration done

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import Link from 'next/link';
+
 const Header = () => {
     return (
         <div className='header__main'>
-            <p>I'm a header</p>
+            <nav>
+                <Link href='/'><a className=''>Home</a></Link>
+                <Link href='/about'><a className=''>A propos</a></Link>
+                <Link href='/projets'><a className=''>Projets</a></Link>
+            </nav>
         </div>
     );
 }
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Index = () => {
     return (
         <div>
-            <h1>I'm a service page</h1>
+
         </div>
     );
 }
